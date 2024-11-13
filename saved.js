8e1e7a849ac6a0c7a9e211f9a496258b12fc33a6
Mhall258 <127550712+Mhall258@users.noreply.github.com>
2024-11-12 19:27:42 -0600

    y
    good

--- saved.js
+++
@@ -1,9 +0,0 @@
-
-function showAddedToCartMessage() {
-    const messageElement = document.getElementById("addedToCart");
-    messageElement.classList.remove("hidden");
-
-    setTimeout(() => {
-        messageElement.classList.add("hidden");
-    }, 2000);
-}
